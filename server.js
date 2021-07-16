c783fc9904657bb3bfed19ad398de63e50016fea
isaac_lau <42047110+pileofscraps@users.noreply.github.com>
2021-07-16 14:07:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ function notificationReceived(req) {
 // add an address to a notification in Alchemy
 async function addAddress(new_address) {
   console.log("adding address " + new_address);
-  const body = { webhook_id: 149506, addresses_to_add: [new_address], addresses_to_remove: [] };
+  const body = { webhook_id: <webhook-id>, addresses_to_add: [new_address], addresses_to_remove: [] };
   try {
     fetch('https://dashboard.alchemyapi.io/api/update-webhook-addresses', {
       method: 'PATCH',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
-      headers: { 'X-Alchemy-Token': ExPFJ6OjpIrYEuerVKxglAigbwA7C7bz}
+      headers: { 'X-Alchemy-Token': <Alchemy-auth-key>}
     })
       .then(res => res.json())
       .then(json => console.log(json));
